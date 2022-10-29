11bf19df1f90838ec16b5ee9e43bfe53e62752fd
Lukas Ames <lames1@my.centennialcollege.ca>
2022-10-28 20:17:20 -0400

    Completed

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,2 @@
-export const MongoURI = "mongodb://localhost:27017/media";
+export const MongoURI = "mongodb+srv://Lukas337:z4i7vLEKiLULERg@cluster0.9d1pdla.mongodb.net/midterm?retryWrites=true&w=majority";
 export const Secret = "someSecret";
